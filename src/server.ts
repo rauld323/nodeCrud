ca6b0c383122fef8fa0f0663da30012d5c406fc7
Raul Davila <rauldavila323@gmail.com>
2023-09-29 17:25:14 +0200

    practice with middle-ware

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,19 @@
 import express from "express";
 import router from "./router";
+import morgan from "morgan";
 
 const app = express();
 
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  req.shhh_secret = "doggy";
+  next();
+});
+
 app.get("/", (req, res) => {
-  console.log("hello from express");
   res.status(200);
   res.json({ message: "hello" });
 });
